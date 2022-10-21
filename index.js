222506aee1a4ee44f81b3525bb549b69317b0b40
bartlomiej <klamerski.bartlomiej@gmail.com>
2022-10-21 23:14:53 +0200

    aaaaaaa

--- index.js
+++ index.js
@@ -15,8 +15,25 @@ function createRoom(roomData) {
         }
         return { type: 'createResult', data: 'incorrect password' };
     }
-    rooms.set(roomData.name, roomData);
+    rooms.set(roomData.name, {...roomData, refereePositions: []});
     return { type: 'createResult', data: { result: 'room created', name: roomData.name} };
+}
+
+function refereeJoin(data) {
+    const room = rooms.get(data.name);
+    if(room) {
+        if(data.password === room.password) {
+            if(room.refereePositions.includes(data.refereePosition)) {
+                return { type: 'joinResult', data: 'pozycja już zajęta'}
+            } else {
+                room.refereePositions.push(data.refereePosition);
+                return { type: 'joinResult', data: data.refereePosition};
+            }
+        } else {
+            return { type: 'joinResult', data:'nieprawidłowe hasło'};
+        }
+    }
+    return {type: 'joinResult', data:'Zawody nie istnieją'}
 }
 
 app.get('/', function(req, res){
@@ -29,6 +46,8 @@ app.ws('/', function(ws, req){
         console.log(msgJson);
         if(msgJson.type === 'create') {
             ws.send(JSON.stringify(createRoom(msgJson.data)));
+        } else if(msgJson.type === 'ref join') {
+            ws.send(JSON.stringify(refereeJoin(msgJson.data)));
         }
     });
 });
