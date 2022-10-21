8f85edc909e39345610058514c85c65d0438d156
bartlomiej <klamerski.bartlomiej@gmail.com>
2022-10-21 19:31:22 +0200

    stworzenie serwera

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const expressWs = require('express-ws')(app);
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-    res.send('Hello World')
+    res.sendFile(path.join(__dirname+'/public/index.html'))
 })
+
+app.ws('/', function(ws, req){
+    ws.on('message', function(msg){
+        console.log(msg);
+    });
+});
 
 app.listen(PORT)
 
