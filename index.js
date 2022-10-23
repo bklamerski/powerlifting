dac930a27cbf17547375aec335266028c330f8de
Adrian Ałaszewski <wns2148810@interia.pl>
2022-10-23 16:32:40 +0200

    gotowe

--- index.js
+++ index.js
@@ -1,57 +1,88 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const expressWs = require('express-ws')(app);
+const express = require("express");
+const app = express();
+const path = require("path");
+const expressWs = require("express-ws")(app);
 
 const PORT = process.env.PORT || 3000;
 
 const rooms = new Map();
 
-function createRoom(roomData) {
-    const room = rooms.get(roomData.name);
-    if(room) {
-        if(roomData.password === room.password) {
-            return { type: 'createResult', data: 'room already exists' };
-        }
-        return { type: 'createResult', data: 'incorrect password' };
+function createRoom(roomData, ownerWs) {
+  const room = rooms.get(roomData.name);
+  if (room) {
+    if (roomData.password === room.password) {
+      room.ownerWs = ownerWs;
+      return { type: "createResult", data: "room already exists" };
     }
-    rooms.set(roomData.name, {...roomData, refereePositions: []});
-    return { type: 'createResult', data: { result: 'room created', name: roomData.name} };
+    return { type: "createResult", data: "incorrect password" };
+  }
+  rooms.set(roomData.name, {
+    ...roomData,
+    refereePositions: [],
+    ownerWs,
+    votes: [],
+  });
+  return {
+    type: "createResult",
+    data: { result: "room created", name: roomData.name },
+  };
 }
 
 function refereeJoin(data) {
-    const room = rooms.get(data.name);
-    if(room) {
-        if(data.password === room.password) {
-            if(room.refereePositions.includes(data.refereePosition)) {
-                return { type: 'joinResult', data: 'pozycja już zajęta'}
-            } else {
-                room.refereePositions.push(data.refereePosition);
-                return { type: 'joinResult', data: data.refereePosition};
-            }
-        } else {
-            return { type: 'joinResult', data:'nieprawidłowe hasło'};
-        }
+  const room = rooms.get(data.name);
+  if (room) {
+    if (data.password === room.password) {
+      if (room.refereePositions.includes(data.refereePosition)) {
+        return { type: "joinResult", data: "pozycja już zajęta" };
+      } else {
+        room.refereePositions.push(data.refereePosition);
+        return { type: "joinResult", data: data.refereePosition };
+      }
+    } else {
+      return { type: "joinResult", data: "nieprawidłowe hasło" };
     }
-    return {type: 'joinResult', data:'Zawody nie istnieją'}
+  }
+  return { type: "joinResult", data: "Zawody nie istnieją" };
 }
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'/public/index.html'))
-})
+function refereeVote(data) {
+  const room = rooms.get(data.contest);
+  if (room) {
+    if (room.votes.some((vote) => vote.refPosition === data.position)) {
+      return { type: "refVoteResponse", data: "already voted" };
+    }
 
-app.ws('/', function(ws, req){
-    ws.on('message', function(msg){
-        const msgJson = JSON.parse(msg)
-        console.log(msgJson);
-        if(msgJson.type === 'create') {
-            ws.send(JSON.stringify(createRoom(msgJson.data)));
-        } else if(msgJson.type === 'ref join') {
-            ws.send(JSON.stringify(refereeJoin(msgJson.data)));
-        }
-    });
+    room.votes.push({ refPosition: data.position, decision: data.decision });
+
+    if (room.votes.length === 3) {
+      room.ownerWs.send(
+        JSON.stringify({ type: "votingResult", data: room.votes })
+      );
+      room.votes.splice(0, room.votes.length);
+    }
+    return { type: "refVoteResponse", data: "vote success" };
+  }
+  return { type: "refVoteResponse", data: "room doesnt exist, vote failed" };
+}
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-app.listen(PORT)
+app.ws("/", function (ws, req) {
+  ws.on("message", function (msg) {
+    const msgJson = JSON.parse(msg);
+    console.log(msgJson);
+    if (msgJson.type === "create") {
+      ws.send(JSON.stringify(createRoom(msgJson.data, ws)));
+    } else if (msgJson.type === "ref join") {
+      ws.send(JSON.stringify(refereeJoin(msgJson.data)));
+    } else if (msgJson.type === "ref-vote") {
+      ws.send(JSON.stringify(refereeVote(msgJson.data)));
+    }
+  });
+});
 
-console.log("start")
+app.listen(PORT);
+
+console.log("start");
